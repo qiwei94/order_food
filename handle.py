8b9194f4d8cc0a931a6341f34aa0f4a7ce820e27
qiwei94 <qiweizhang8523@gmail.com>
2018-12-11 22:03:36 +0800

    fix handle.py

--- handle.py
+++ handle.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-# filename: handle.pyimport hashlibimport replyimport receiveimport webclass Handle(object):
-    def POST(self):
-        try:
-            webData = web.data()
-            print "Handle Post webdata is ", webData
-   #后台打日志
-            recMsg = receive.parse_xml(webData)
-            if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
-                toUser = recMsg.FromUserName
-                fromUser = recMsg.ToUserName
-                content = "test"
-                replyMsg = reply.TextMsg(toUser, fromUser, content)
-                return replyMsg.send()
-            else:
-                print "暂且不处理"
-                return "success"
-        except Exception, Argment:
-            return Argment
+def POST(self):
+    try:
+        webData = web.data()
+        print "Handle Post webdata is ", webData
+        #后台打日志
+        recMsg = receive.parse_xml(webData)
+        if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
+            toUser = recMsg.FromUserName
+            fromUser = recMsg.ToUserName
+            content = "test"
+            replyMsg = reply.TextMsg(toUser, fromUser, content)
+            return replyMsg.send()
+        else:
+            print "暂且不处理"
+            return "success"
+    except Exception, Argment:
+        return Argment
